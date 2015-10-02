367191914a9921ba21b710d32eb7fbf084a423ad
Przemysław Fałowski <przemkow92@gmail.com>
2015-10-02 15:45:05 +0100

    add fields to Video, add Tag interface

--- src/tuVes/Player.java
+++ src/tuVes/Player.java
@@ -9,13 +9,13 @@ public interface Player {
 	public void insertVideo(StringTokenizer idVideo, StringTokenizer nick, StringTokenizer URL, long length, String title);
 	public void disableVideo(StringTokenizer idVideo);
 	public void playVideo(StringTokenizer idVide, StringTokenizer nick);
-	public Iterator<String> listHistory(StringTokenizer nick);
+	public Iterator<String> listHistoryIterator(StringTokenizer nick);
 	public void removeHistory(StringTokenizer nick);
 	public void addVideoToFavourites(StringTokenizer idVideo, StringTokenizer nick);
 	public void removeVideoFromFavourites(StringTokenizer idVideo, StringTokenizer nick);
-	public Iterator<String> listFavourites(StringTokenizer nick);
+	public Iterator<String> listFavouritesIterator(StringTokenizer nick);
 	public void addTagToVideo(StringTokenizer idVideo, StringTokenizer tag);
-	public Iterator<String> listTags(StringTokenizer idVideo);
+	public Iterator<String> listTagsIterator(StringTokenizer idVideo);
 	public void searchTag(StringTokenizer tag);
 	public void load();
 	public void save();
--- src/tuVes/Video.java
+++ src/tuVes/Video.java
@@ -1,5 +1,10 @@
 package tuVes;
 
+import java.util.Iterator;
+
 public interface Video {
-	
+	public void disableVideo();
+	public boolean isVideoDisabled();
+	public void addTagToVideo();
+	public Iterator<String> listTagsIterator();
 }
